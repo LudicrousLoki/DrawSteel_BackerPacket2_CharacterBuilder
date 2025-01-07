7b8f1a0dcbe0770520ce1626270895ba488640b2
LudicrousLoki <snaticallegari@gmail.com>
2025-01-06 23:25:40 -0500

    Switched back to vscode

---
+++ app/src/main/kotlin/KotlinApp.kt
@@ -0,0 +1,11 @@
+package drawsteel_backerpacket2_characterbuilder;
+
+class KotlinApp {
+    public fun getGreeting() : String {
+        return "Kotlin";
+    }
+}
+
+fun main(){
+    println(KotlinApp().getGreeting())
+}
